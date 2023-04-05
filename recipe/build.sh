64ed3c24ff27d8a28e061033176105b19d01a5a6
dipterix <dipterix.wang@gmail.com>
2023-04-05 19:34:35 -0400

    added (de)activate for windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/scripts/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
 
 
